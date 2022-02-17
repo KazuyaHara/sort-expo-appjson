3096a133ef4404acd5328f0fc7f05bf7cc0c2818
Kazuya Hara <iam@kazuyahara.dev>
2022-02-17 16:58:24 +0900

    fix: use latest version as default

--- src/versions.ts
+++ src/versions.ts
@@ -1,2 +1,2 @@
 export const versions = ["40.0.0", "41.0.0", "42.0.0", "43.0.0", "44.0.0"];
-export const defaultVersion = versions.slice(-1)[0];
+export const defaultVersion = versions.sort().slice(-1)[0];
